8a39074b6e135207ec2fc9ac4fa3b88c8c0bbbfe
James Gallagher <jgallagher@opendap.org>
2024-11-20 11:49:30 -0700

    Moving files the modifier code does not use to 'retired.'

--- errLog.py
+++ errLog.py
@@ -3,11 +3,10 @@ import time
 
 def output_errlog(msg):
     """
-    error logging function
+    error logging function - write a message to a date-stamped file.
     :param msg:
     :return:
     """
     errlogfile = "logs/pydmrError" + time.strftime("-%m.%d.%Y") + ".log"
     with open(errlogfile, "a") as f:
         f.write(msg)
-    f.close()
